ac27f529c36fb40f8f05fb2c2ed26d88dcc23c77
wnsdud820 <wnsdud040820@gmail.com>
2021-04-24 20:06:11 +0900

    fisbug2

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ SERVER MEMBERS INTENT 활성화를 필요로 합니다.
 const Discord = require("discord.js")
 const intent_list = new Discord.Intents(["GUILD_MEMBERS", "GUILD_MESSAGES", "GUILDS", "GUILD_INVITES"])
 const client = new Discord.Client({ ws: { intents: intent_list } })
-const moment = require("moment")
-require("moment-duration-format")
-const momenttz = require("moment-timezone")
-const token = "process.env.token" // heroku를 사용하지 않을꺼라면 const token = "디스코드 봇 토큰" 으로 바꿔주세요.
+const token = process.argv.length == 2 ? process.env.token : "" // heroku를 사용하지 않을꺼라면 const token = "디스코드 봇 토큰" 으로 바꿔주세요.
 const welcomeChannelName = "안녕하세요" // 입장 시 환영메시지를 전송 할 채널의 이름을 입력하세요.
 const byeChannelName = "안녕히가세요" // 퇴장 시 메시지를 전송 할 채널의 이름을 입력하세요.
 const welcomeChannelComment = "어서오세요." // 입장 시 전송할 환영메시지의 내용을 입력하세요.
@@ -36,7 +33,7 @@ client.on("guildMemberAdd", (member) => {
   const welcomeChannel = guild.channels.cache.find((channel) => channel.name == welcomeChannelName)
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`) // 올바른 채널명을 기입하지 않았다면, Cannot read property 'send' of undefined; 오류가 발생합니다.
-  member.roles.add(guild.roles.cache.find((role) => role.name === roleName).id)
+  member.roles.add(guild.roles.cache.find((r) => r.name === roleName).id)
 })
 
 client.on("guildMemberRemove", (member) => {
@@ -54,7 +51,7 @@ client.on("message", (message) => {
     return message.reply("pong")
   }
 
-  if (message.content == "!si") {
+  if (message.content == "!is") {
     let embed = new Discord.MessageEmbed()
     let img = "https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256"
     var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]")
@@ -91,11 +88,13 @@ client.on("message", (message) => {
       .setURL("http://www.naver.com")
       .setAuthor("나긋해", img, "http://www.naver.com")
       .setThumbnail(img)
+      ////.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다. 
       .addField("Inline field title", "Some value here")
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here1\nSome value here2\nSome value here3\n")
+      ////.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.  
       .setTimestamp()
       .setFooter("나긋해가 만듬", img)
 
@@ -103,14 +102,10 @@ client.on("message", (message) => {
   } else if (message.content == "!help") {
     let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
     let commandList = [
-      { name: "!help", desc: "help" },
       { name: "ping", desc: "현재 핑 상태" },
       { name: "embed", desc: "embed 예제1" },
+      { name: "embed2", desc: "embed 예제2 (help)" },
       { name: "!전체공지", desc: "dm으로 전체 공지 보내기" },
-      { name: "!전체공지2", desc: "dm으로 전체 embed 형식으로 공지 보내기" },
-      { name: "!청소", desc: "텍스트 지움" },
-      { name: "!초대코드", desc: "해당 채널의 초대 코드 표기" },
-      { name: "!초대코드2", desc: "봇이 들어가있는 모든 채널의 초대 코드 표기" },
     ]
     let commandStr = ""
     let embed = new Discord.MessageEmbed().setAuthor("Help of 콜라곰 BOT", helpImg).setColor("#186de6").setFooter(`콜라곰 BOT ❤️`).setTimestamp()
@@ -122,35 +117,6 @@ client.on("message", (message) => {
     embed.addField("Commands: ", commandStr)
 
     message.channel.send(embed)
-  } else if (message.content == "!초대코드2") {
-    client.guilds.cache.array().forEach((x) => {
-      x.channels.cache
-        .find((x) => x.type == "text")
-        .createInvite({ maxAge: 0 }) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-        .then((invite) => {
-          message.channel.send(invite.url)
-        })
-        .catch((err) => {
-          if (err.code == 50013) {
-            message.channel.send(`**${x.channels.cache.find((x) => x.type == "text").guild.name}** 채널 권한이 없어 초대코드 발행 실패`)
-          }
-        })
-    })
-  } else if (message.content == "!초대코드") {
-    if (message.channel.type == "dm") {
-      return message.reply("dm에서 사용할 수 없는 명령어 입니다.")
-    }
-    message.guild.channels.cache
-      .get(message.channel.id)
-      .createInvite({ maxAge: 0 }) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-      .then((invite) => {
-        message.channel.send(invite.url)
-      })
-      .catch((err) => {
-        if (err.code == 50013) {
-          message.channel.send(`**${message.guild.channels.cache.get(message.channel.id).guild.name}** 채널 권한이 없어 초대코드 발행 실패`)
-        }
-      })
   } else if (message.content.startsWith("!전체공지2")) {
     if (checkPermission(message)) return
     if (message.member != null) {
@@ -168,8 +134,25 @@ client.on("message", (message) => {
       return message.reply("공지를 전송했습니다.")
     } else {
       return message.reply("채널에서 실행해주세요.")
+    }  
+  } else if (message.content == "!초대코드") {
+    if (message.channel.type == "dm") {
+      return message.reply("dm에서 사용할 수 없는 명령어 입니다.")
     }
-  } else if (message.content.startsWith("!전체공지")) {
+    message.guild.channels.cache
+      .get(message.channel.id)
+      .createInvite({ maxAge: 0 }) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+      .then((invite) => {
+        message.channel.send(invite.url)
+      })
+      .catch((err) => {
+        if (err.code == 50013) {
+          message.channel.send(`**${message.guild.channels.cache.get(message.channel.id).guild.name}** 채널 권한이 없어 초대코드 발행 실패`)
+        }
+      })
+  }
+
+  if (message.content.startsWith("!전체공지")) {
     if (checkPermission(message)) return
     if (message.member != null) {
       // 채널에서 공지 쓸 때
@@ -183,7 +166,9 @@ client.on("message", (message) => {
     } else {
       return message.reply("채널에서 실행해주세요.")
     }
-  } else if (message.content.startsWith("!청소")) {
+  }
+
+  if (message.content.startsWith("!청소")) {
     if (message.channel.type == "dm") {
       return message.reply("dm에서 사용할 수 없는 명령어 입니다.")
     }
