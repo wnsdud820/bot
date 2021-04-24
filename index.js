3013bf11e95c7cd475c9593d0153a5e46e17858a
wnsdud820 <wnsdud040820@gmail.com>
2021-04-24 19:25:12 +0900

    add clear command

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const Discord = require("discord.js")
+/*
+[ 필수 확인 ]
+
+본 코드는 나긋해님의 코드를 Discord.js v12에 맞게 변경한 코드이며,
+SERVER MEMBERS INTENT 활성화를 필요로 합니다.
+
+봇 토큰을 발급받는 페이지에서 하단으로 스크롤하면 Privileged Gateway Intents 라는 항목이 있습니다.
+해당 항목 중 SERVER MEMBERS INTENT 를 활성화 해주시면 됩니다.
+
+활성화가 됬다면 우측 버튼이 파란색으로 바뀝니다.
+
+만약 활성화하지 않고 봇을 키시면 켜지지 않습니다.
+*/
+
+const Discord = require("discord.js")  
 const intent_list = new Discord.Intents(["GUILD_MEMBERS", "GUILD_MESSAGES", "GUILDS", "GUILD_INVITES"])
 const client = new Discord.Client({ ws: { intents: intent_list } })
-const token = process.env.token
+const token = process.argv.length == 2 ? process.env.token : "" // heroku를 사용하지 않을꺼라면 const token = "디스코드 봇 토큰" 으로 바꿔주세요.
 const welcomeChannelName = "안녕하세요" // 입장 시 환영메시지를 전송 할 채널의 이름을 입력하세요.
 const byeChannelName = "안녕히가세요" // 퇴장 시 메시지를 전송 할 채널의 이름을 입력하세요.
 const welcomeChannelComment = "어서오세요." // 입장 시 전송할 환영메시지의 내용을 입력하세요.
@@ -43,13 +57,13 @@ client.on("message", (message) => {
       .setURL("http://www.naver.com")
       .setAuthor("나긋해", img, "http://www.naver.com")
       .setThumbnail(img)
-      //.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
+      ////.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
       .addField("Inline field title", "Some value here")
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here", true)
       .addField("Inline field title", "Some value here1\nSome value here2\nSome value here3\n")
-      //.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
+      ////.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
       .setTimestamp()
       .setFooter("나긋해가 만듬", img)
 
@@ -89,11 +103,53 @@ client.on("message", (message) => {
       return message.reply("채널에서 실행해주세요.")
     }
   }
+
+  if (message.content.startsWith("!청소")) {
+    if (message.channel.type == "dm") {
+      return message.reply("dm에서 사용할 수 없는 명령어 입니다.")
+    }
+
+    if (message.channel.type != "dm" && checkPermission(message)) return
+
+    var clearLine = message.content.slice("!청소 ".length)
+    var isNum = !isNaN(clearLine)
+
+    if (isNum && (clearLine <= 0 || 100 < clearLine)) {
+      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+      return
+    } else if (!isNum) {
+      // c @나긋해 3
+      if (message.content.split("<@").length == 2) {
+        if (isNaN(message.content.split(" ")[2])) return
+
+        var user = message.content.split(" ")[1].split("<@!")[1].split(">")[0]
+        var count = parseInt(message.content.split(" ")[2]) + 1
+        let _cnt = 0
+
+        message.channel.messages.fetch().then((collected) => {
+          collected.every((msg) => {
+            if (msg.author.id == user) {
+              msg.delete()
+              ++_cnt
+            }
+            return !(_cnt == count)
+          })
+        })
+      }
+    } else {
+      message.channel
+        .bulkDelete(parseInt(clearLine) + 1)
+        .then(() => {
+          message.channel.send(`<@${message.author.id}> ${parseInt(clearLine)} 개의 메시지를 삭제했습니다. (이 메시지는 잠시 후 사라집니다.)`).then((msg) => msg.delete({ timeout: 3000 }))
+        })
+        .catch(console.error)
+    }
+  }
 })
 
 function checkPermission(message) {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    message.channel.send(`<@${message.author.id}> 명령어를 수행할 관리자 권한을 소지하고 있지않습니다.`)
     return true
   } else {
     return false
